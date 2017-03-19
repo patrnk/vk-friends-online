ca59dc00b9e58dbf45fc93a4e789464c643f4794
Paul Vergeev <paulvergeev@gmail.com>
2017-03-19 12:56:29 +0300

    import vk_api the right way

--- app/vk/friends_online.py
+++ app/vk/friends_online.py
@@ -1,6 +1,4 @@
-from .vk_api import invoke_with_cooldown
-from .vk_api import fetch_online_friend_ids
-from .vk_api import fetch_user
+from . import vk_api
 
 
 def extract_username_from_url(user_url):
@@ -9,13 +7,14 @@ def extract_username_from_url(user_url):
 
 
 def fetch_online_friends(access_token, user_id=None):
-    online_friend_ids = fetch_online_friend_ids(access_token, user_id)
+    online_friend_ids = vk_api.fetch_online_friend_ids(access_token, user_id)
     online_desktop_ids = online_friend_ids['online']
     online_mobile_ids = online_friend_ids['online_mobile']
     online_friends = []
     for online_id in online_desktop_ids + online_mobile_ids:
-        user = invoke_with_cooldown(fetch_user, access_token=access_token, 
-                                    username=online_id)
+        # FIXME: employ a different approach
+        user = vk_api.invoke_with_cooldown(vk_api.fetch_user, access_token=access_token, 
+                                           username=online_id)
         name = ' '.join((user['first_name'], user['last_name']))
         online_friends.append({'name': name,
                                'url': 'https://vk.com/id%d' % online_id,
