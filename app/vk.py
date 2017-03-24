c28ad2b611bd0bb672ec24dab67adf7d3e45838c
Paul Vergeev <paulvergeev@gmail.com>
2017-03-24 16:25:07 +0300

    Close #7
    This is a quashed commit of the following:
    
    commit 45cd03246213b70d5d565d4b6f77ecb2c2e81b7d
    Author: Paul Vergeev <paulvergeev@gmail.com>
    Date:   Fri Mar 24 16:21:19 2017 +0300
    
        fix the use of nonexistent variable
    
    commit 6c9d1a0085a956be0dc5d5e42102fd37fc18b879
    Author: Paul Vergeev <paulvergeev@gmail.com>
    Date:   Fri Mar 24 16:18:42 2017 +0300
    
        close #7
    
        As a consequence, the information about user's device is removed.

--- app/vk.py
+++ app/vk.py
@@ -50,8 +50,7 @@ def form_authorization_url(redirect_uri):
               'scope': 'friends',
               'response_type': 'code',
               }
-    request = requests.Request('GET', 'https://oauth.vk.com/authorize',
-                               params=params)
+    request = requests.Request('GET', 'https://oauth.vk.com/authorize', params=params)
     request.prepare()
     return request.prepare().url
 
@@ -84,38 +83,25 @@ def fetch_user(access_token, username=None, name_case='nom'):
     return user_list[0]
 
 
-def fetch_friend_names(access_token, count, user_id=None, offset=0):
+def fetch_friends(access_token, count, user_id=None, offset=0):
     ''' For thorough description of the request parameters, refer to
         friends.get documentation: https://vk.com/dev/friends.get
     '''
     params = {'user_id': user_id,
               'access_token': access_token,
-              'fields': 'nickname',  # this way we can get the names of the friends
+              'fields': 'online',
               'count': count,
               'offset': offset,
               }
     return make_vk_api_request('friends.get', **params)['response']['items']
 
 
-def fetch_friend_list(access_token, user_id=None):
+def fetch_full_friend_list(access_token, user_id=None):
     max_friends = 5000  # VK won't return more because of 'fields' parameter
-    friends = fetch_friend_names(access_token, max_friends, user_id)
+    friends = fetch_friends(access_token, max_friends, user_id)
     if len(friends) == max_friends:
-        friends += fetch_friend_names(access_token, max_friends, user_id, 
-                                      offset=max_friends)
+        friends += fetch_friends(access_token, max_friends, user_id, offset=max_friends)
     return friends
-
-
-def fetch_online_friend_ids(access_token, user_id=None, online_mobile=1):
-    ''' For thorough description of the request parameters, refer to
-        friends.getOnline documentation: https://vk.com/dev/friends.getOnline
-    '''
-    params = {'user_id': user_id, 
-              'online_mobile': 1,  # separate mobile online from desktop online
-              'access_token': access_token,
-              }
-    online_friend_ids = make_vk_api_request('friends.getOnline', **params)
-    return online_friend_ids['response']
 
 
 def extract_username_from_url(user_url):
@@ -126,24 +112,13 @@ def extract_username_from_url(user_url):
     return username
 
 
-def turn_friend_list_into_dictionary(friend_list):
-    friends = {}
-    for friend in friend_list:
-        friends[friend['id']] = friend
-    return friends
-
-
 def fetch_online_friends(access_token, user_id=None):
-    friend_list = fetch_friend_list(access_token, user_id)
-    friends = turn_friend_list_into_dictionary(friend_list)
-    online_friend_ids = fetch_online_friend_ids(access_token, user_id)
-    online_desktop_ids = online_friend_ids['online']
-    online_mobile_ids = online_friend_ids['online_mobile']
+    friend_list = fetch_full_friend_list(access_token, user_id)
     online_friends = []
-    for online_id in online_desktop_ids + online_mobile_ids:
-        friend = friends[online_id]
+    for friend in friend_list:
+        if friend['online'] != 1:
+            continue
         friend_name = ' '.join((friend['first_name'], friend['last_name']))
-        online_friends.append({'name': friend_name,
-                               'url': 'https://vk.com/id%d' % online_id,
-                               'mobile': online_id in online_mobile_ids})
+        friend_url = 'https://vk.com/id%d' % friend['id']
+        online_friends.append({'name': friend_name, 'url': friend_url})
     return online_friends
